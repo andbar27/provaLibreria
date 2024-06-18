6997fcff6a18366082206c0f80b7a7e175790f08
elite-23 <lucacavalleri007@gmail.com>
2024-06-18 13:59:55 +0200

    Link urls dinamico per libro

--- libreria/urls.py
+++ libreria/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from .models import Libro
 
 urlpatterns = [
     # localhost:8000/libreria
     path('', views.libreria, name='home'),
     path('aggiungi_libro', views.aggiungi_libro, name='aggiungi_libro'),
 ]
+for i in Libro.objects.all():
+    urlpatterns.append(path(i.title.replace(" ","-").lower(), views.libro, name=i.title))
