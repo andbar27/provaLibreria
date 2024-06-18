72f6bbcd6795b9bb7cd398adeb21226245319975
andbar27 <andrea.barbato2796@icloud.com>
2024-06-18 13:34:58 +0200

    minor

--- libreria/froms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from libreria.models import Libro
-
-class LibroForm(forms.ModelForm):
-
-    class Meta:
-        model = Libro
-        fields = ['book_id', 'image', 'title', 'author', 'is_borrowed', 'is_expired']
