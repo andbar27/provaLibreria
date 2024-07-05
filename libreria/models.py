c61b767efb321a772e7c900bba667dafd3d26b5d
andbar27 <andrea.barbato2796@icloud.com>
2024-07-05 18:46:40 +0200

    minor

--- libreria/models.py
+++ libreria/models.py
@@ -3,6 +3,12 @@ from django.db import models
 from django.core.validators import MinLengthValidator
 
 # Create your models here.
+class Membro(models.Model):
+    member_id = models.CharField(max_length=13, primary_key=True)
+    name = models.CharField(max_length=40)
+
+    def __str__(self) -> str:
+        return self.name
 
 class Libro(models.Model):
     book_id = models.CharField(max_length=13, validators=[MinLengthValidator(13)], primary_key=True)
@@ -12,6 +18,8 @@ class Libro(models.Model):
     is_borrowed = models.BooleanField(default=False)
     is_expired = models.BooleanField(default=False)
 
+    member = models.ForeignKey(Membro, on_delete=models.SET_NULL, null=True, blank=True, related_name='members')
+
     def __str__(self) -> str:
         return self.title
     
@@ -19,9 +27,3 @@ class Libro(models.Model):
         return self.book_id
     
 
-class Membro(models.Model):
-    member_id = models.CharField(max_length=13, primary_key=True)
-    name = models.CharField(max_length=40)
-
-    def __str__(self) -> str:
-        return self.name
