25f680efb6e2b3104c1b227b4012cfc5e59c54da
andbar27 <andrea.barbato2796@icloud.com>
2024-07-19 18:35:04 +0200

    minor

--- libreria/urls.py
+++ libreria/urls.py
@@ -4,14 +4,15 @@ from .models import Libro, Membro
 
 urlpatterns = [
     path('', views.libreria, name='home'),                                          # localhost:8000/libreria
-    path('stats_libreria', views.statistiche_libreria, name='stats_library'), 
-    path('add-book', views.aggiungi_libro, name='aggiungi_libro'),
-    path('add-member', views.aggiungi_membro, name='aggiungi_membro'),
-    path('list-members', views.lista_membri, name='lista_membri'),
+    path('stats_libreria', views.statistiche_libreria, name='stats_library'),       # Stats library page
+    path('add-book', views.aggiungi_libro, name='aggiungi_libro'),                  # Add book page
+    path('add-member', views.aggiungi_membro, name='aggiungi_membro'),              # Add member page
+    path('list-members', views.lista_membri, name='lista_membri'),                  # List member page
     path('<str:book_id>', views.libro, name="book"),                                # The book details page
-    path('<str:book_id>/modify', views.modifica_libro, name="book-modify"),         # The profile page
+    path('<str:book_id>/modify', views.modifica_libro, name="book-modify"),         # Edit book page
     path('m/<str:member_id>', views.membro, name="member"),                         # The profile page
     path('m/<str:member_id>/modify', views.modifica_membro, name="member-modify"),  # Edit profile page
+                                                                                    # Return book url
     path('membro/<str:member_id>/return_book/<str:book_id>/', views.restituisci_libro, name='return_book'),
       
 ]
