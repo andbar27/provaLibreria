84800a4d695ed9f394c7270bf90ecab409759234
andbar27 <andrea.barbato2796@icloud.com>
2025-03-06 20:05:45 +0100

    python3

--- start_django.sh
+++ start_django.sh
@@ -11,5 +11,5 @@ cd /home/salotto/proj/provaLibreria || { echo "Cartella non trovata!"; exit 1; }
 
 # Avviare il server Django
 echo "Avvio del server Django su http://127.0.0.1:$PORT/"
-python manage.py runserver 0.0.0.0:$PORT
+python3 manage.py runserver 0.0.0.0:$PORT
 
