548ed61dadf0b2eb9d95c92dcd876fbf28a9d5f3
andbar27 <andrea.barbato2796@icloud.com>
2025-03-09 23:31:52 +0100

    aggiunta category

--- libreria/views.py
+++ libreria/views.py
@@ -12,7 +12,8 @@ def libreria(request):
     
     if form.is_valid():
         query = form.cleaned_data['query'].lower()
-        libri = [libro for libro in libri if query in libro.title.lower() or query in libro.author.lower() or query == libro.book_id]
+        libri = [libro for libro in libri if query in libro.title.lower() or query in libro.author.lower() \
+                 or query in libro.category.lower() or query in libro.book_id]
     
     context = {'libri':libri, 'form': form}
 
